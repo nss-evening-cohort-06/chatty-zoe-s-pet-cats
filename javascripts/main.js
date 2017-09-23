ff8a4802c2b8d744c5ad5ce234591449e851975b
Anna Dolan <anna.dolan115@gmail.com>
2017-09-23 12:42:46 -0500

    xhr working, need to print to dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,13 @@
-"use strict"; 
 
+"use strict";
 
-
-
+const doItAll = require('./data');
 const runEvents = require("./events");
+
 
 
 
 
 runEvents.submitMessage();
 runEvents.writeWordsOnEnter();
-runEvents.clearTextBox();
-runEvents.deleteMessage();
+
