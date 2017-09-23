44835dfe09b6f2435e78db84a4d0fae9d5d47e9a
David Dassau <todieahero@hotmail.com>
2017-09-23 09:56:24 -0500

    moved twilight checkbox event from main.js to events.js

--- javascripts/events.js
+++ javascripts/events.js
@@ -27,6 +27,27 @@ const writeWordsOnEnter = (e) => {
 
 
 
+// JS for "Twilight Mode" checkbox
+let outputDark = document.getElementById('output-dark');
+let checkbox = document.querySelector("input[name=checkbox]");
+
+checkbox.addEventListener( 'change', function() {
+    if(this.checked) {
+        // Checkbox is checked..
+        outputDark.classList.add("twilight-zone-mode");
+    } else {
+        // Checkbox is not checked..
+        outputDark.classList.remove("twilight-zone-mode");
+    }
+});
+
+
+
+
+// JS for "Make Text Bigger" checkbox
+
+
+
 
 
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -6,23 +6,7 @@
 const runEvents = require("./events");
 
 
-let outputDark = document.getElementById('output-dark');
-let checkbox = document.querySelector("input[name=checkbox]");
 
-checkbox.addEventListener( 'change', function() {
-    if(this.checked) {
-        // Checkbox is checked..
-        outputDark.classList.add("twilight-zone-mode");
-    } else {
-        // Checkbox is not checked..
-        outputDark.classList.remove("twilight-zone-mode");
-    }
-});
-
-
-
-
-// JS for "Make Text Bigger" checkbox
  
 
 
